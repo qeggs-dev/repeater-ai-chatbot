6afdea2b0fdf24a13cdc59c0e8ddb75b201b809f
Qeggs <get_boot@outlook.com>
2025-11-21 09:12:40 +0800

    fix(core): 修改模型ID字段名
    将 Response 类中的 model_id 字段重命名为 model_uid，以保持命名一致性。

--- core/CoreResponse/_response.py
+++ core/CoreResponse/_response.py
@@ -6,7 +6,7 @@ class Response:
     content: str = ""
     model_name: str = ""
     model_type: str = ""
-    model_id: str = ""
+    model_uid: str = ""
     create_time: int = 0
     id: str = ""
     finish_reason_cause: str = ""
