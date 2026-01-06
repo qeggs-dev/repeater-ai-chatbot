2f772eb7675e5b17ed052ec550ccfcdece114ced
Qeggs <get_boot@outlook.com>
2026-01-06 14:02:29 +0800

    refactor(core/API/_global_exception_handler/_except_handler): 移除未使用的base_exception_handler导入
    移除了_init__.py文件中未使用的_base_exception导入和
    __all__列表中的对应引用

--- core/API/_global_exception_handler/_except_handler/__init__.py
+++ core/API/_global_exception_handler/_except_handler/__init__.py
@@ -1,9 +1,7 @@
 from ._exception import exception_handler
 from ._warning import WarningHandler
-from ._base_exception import base_exception_handler
 
 __all__ = [
     "exception_handler",
     "WarningHandler",
-    "base_exception_handler",
 ]
