f7290b4b6167fc6a85416e26a8646913fef18f11
Qeggs <get_boot@outlook.com>
2026-01-06 15:57:35 +0800

    feat(Global_Exception_Handler): 添加重复回溯配置支持
    - 引入 Repeater_Traceback_Config 模型用于处理重复回溯功能
    - 在 Global_Exception_Handler_Config 中添加 repeater_traceback 字段
    - 配置默认工厂函数以初始化重复回溯配置

--- core/Global_Config_Manager/_models/_global_exception_handler/_global_exception_handler.py
+++ core/Global_Config_Manager/_models/_global_exception_handler/_global_exception_handler.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, ConfigDict, Field
 from ._code_reader import Code_Reader_Config
+from ._repeater_traceback import Repeater_Traceback_Config
 
 class Global_Exception_Handler_Config(BaseModel):
     model_config = ConfigDict(case_sensitive=False)
@@ -12,4 +13,5 @@ class Global_Exception_Handler_Config(BaseModel):
     format_validation_error: bool = True
     record_warnings: bool = True
     record_all_exceptions: bool = False
+    repeater_traceback: Repeater_Traceback_Config = Field(default_factory=Repeater_Traceback_Config)
     code_reader: Code_Reader_Config = Field(default_factory=Code_Reader_Config)
