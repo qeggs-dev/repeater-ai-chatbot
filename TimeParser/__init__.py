56b563f61c5c078fa695e159d30cf58118f13b58
Qeggs <get_boot@outlook.com>
2026-01-25 18:25:00 +0800

    refactor(TimeParser): 重构日期倒计时功能
    将_birthday_countdown模块重命名为_date_countdown，并修改函数名称
    从get_birthday_countdown改为calculation_date_countdown以支持通用目
    标日期计算。更新导入语句并调整函数参数结构，移除特定于生日的功能，
    使倒计时功能更加通用化。

--- TimeParser/__init__.py
+++ TimeParser/__init__.py
@@ -3,7 +3,7 @@ from ._format_deltatime import (
     format_deltatime_ns,
     format_deltatime_high_precision
 )
-from ._birthday_countdown import get_birthday_countdown
+from ._date_countdown import calculation_date_countdown
 from ._zodiac import date_to_zodiac
 from ._format_dimestamp import format_timestamp
 from ._age import calculate_age
