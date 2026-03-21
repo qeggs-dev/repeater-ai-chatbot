52c66c02ef18b7c0d548fe5a6a46fb2c26e4c24e
Qeggs <get_boot@outlook.com>
2026-03-21 16:34:50 +0800

    feat(API): 添加静态文件API的文本编码支持
    新增了对静态文件读取时指定文本编码的支持，允许用户通过查询参数指定文件编码格式。
    同时增强了错误处理机制，添加了文件不存在和目标为目录时的相应HTTP状态码返回。
    引入了aiofiles库进行异步文件操作，并扩展了响应类型以支持纯文本响应。

--- core/API/_apis/_static_api/_static_api.py
+++ core/API/_apis/_static_api/_static_api.py
@@ -1,13 +1,15 @@
+import aiofiles
+
 from ..._resource import Resource
-from fastapi.responses import FileResponse
-from fastapi import HTTPException
+from fastapi.responses import FileResponse, PlainTextResponse
+from fastapi import HTTPException, Query
 from PathProcessors import validate_path
 from pathlib import Path
 
 from ....Global_Config_Manager import ConfigManager
 
 @Resource.app.get("/static/{path:path}")
-async def static_file(path: str):
+async def static_file(path: str, text_encoding: str | None = Query(None)):
     """Return static files"""
     static_dir = Path(ConfigManager.get_configs().static.static_dir)
     if not validate_path(
@@ -15,4 +17,20 @@ async def static_file(path: str):
         new_path=path
     ):
         raise HTTPException(status_code=403, detail="Forbidden")
-    return FileResponse(static_dir / path)
+    
+    base_path = static_dir / path
+    if not base_path.exists():
+        raise HTTPException(status_code=404, detail="Not Found")
+    
+    if base_path.is_dir():
+        raise HTTPException(status_code=403, detail="Target is a directory")
+
+    if text_encoding is None:
+        return FileResponse(static_dir / path)
+    else:
+        async with aiofiles.open(static_dir / path, mode="r", encoding=text_encoding) as f:
+            text = await f.read()
+        return PlainTextResponse(
+            text,
+            status_code = 200,
+        )
