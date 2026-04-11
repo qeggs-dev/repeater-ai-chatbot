d3cf24050bf0423da7019ddd6143f7d8bd2fb24a
Qeggs <get_boot@outlook.com>
2026-04-11 14:02:50 +0800

    feat(core): 添加客户端池日志记录功能
    - 引入loguru作为日志记录工具
    - 在客户端获取过程中添加信息日志
    - 记录缓存客户端的使用情况
    - 记录新客户端的创建过程

--- core/CallAPI/CompletionsAPI/_caller/_client_pool.py
+++ core/CallAPI/CompletionsAPI/_caller/_client_pool.py
@@ -1,5 +1,6 @@
 from ._client_info import ClientInfo
 from openai import AsyncOpenAI
+from loguru import logger
 
 class ClientPool:
     def __init__(self):
@@ -7,8 +8,10 @@ class ClientPool:
     
     def get_client(self, client_info: ClientInfo):
         if client_info in self._client_info:
+            logger.info("Using cached client.")
             return self._client_info[client_info]
         else:
+            logger.info("Creating new client.")
             client = client_info.to_openai_client()
             self._client_info[client_info] = client
             return client
