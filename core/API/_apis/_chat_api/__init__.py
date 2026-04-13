b42b72dcb40109e2b88280749661a6b3079bb45d
Qeggs <get_boot@outlook.com>
2026-04-13 13:59:31 +0800

    feat(chat_api): 新增获取聊天任务ID的API接口
    新增_get_progress_tasks模块，包含get_chat_tasks_id_api接口，
    用于获取聊天任务的进度和ID信息。同时添加了相关的响应模型
    TasksIDResponse和UserInfo，完善了聊天API的功能。

--- core/API/_apis/_chat_api/__init__.py
+++ core/API/_apis/_chat_api/__init__.py
@@ -1,9 +1,11 @@
 from ._chat_api import chat_endpoint
 from ._chat_break_api import chat_break_api
 from ._get_buffer_api import get_chat_buffer_api
+from ._get_progress_tasks import get_chat_tasks_id_api
 
 __all__ = [
     "chat_endpoint",
     "chat_break_api",
     "get_chat_buffer_api",
+    "get_chat_tasks_id_api",
 ]
---
+++ core/API/_apis/_chat_api/_get_progress_tasks/__init__.py
@@ -0,0 +1,8 @@
+from ._get_progress_tasks import get_chat_tasks_id_api
+from ._models import TasksIDResponse, UserInfo
+
+__all__ = [
+    "get_chat_tasks_id_api",
+    "TasksIDResponse",
+    "UserInfo",
+]
