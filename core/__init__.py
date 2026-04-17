8195a34a90b393ac15c5875a7f207279b72010fa
Qeggs <get_boot@outlook.com>
2026-04-17 21:09:39 +0800

    feat(core): 导入API模块并更新导出列表
    - 导入core.api模块的所有内容
    - 扩展__all__列表以包含新导入的功能模块
    - 添加API相关组件到公共接口暴露列表中

--- core/__init__.py
+++ core/__init__.py
@@ -6,3 +6,39 @@ from ._info import (
     __copyright__,
 )
 from .server import Server
+from .api import *
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__license__",
+    "__copyright__",
+    "Core",
+    "Server",
+    "admin_api_key_manager",
+    "api",
+    "assist_struct",
+    "auxiliary",
+    "call_api",
+    "context_manager",
+    "data_manager",
+    "global_config_manager",
+    "licenses_loader",
+    "lifespan",
+    "logger_init",
+    "markdown_render",
+    "model_api",
+    "model_requester",
+    "nexus_client",
+    "pools",
+    "repeater_response",
+    "repeater_traceback",
+    "request_log",
+    "server",
+    "special_exception"
+    "static_resources_client",
+    "status_map",
+    "text_buffer",
+    "text_template_processer",
+    "user_config_manager"
+]
