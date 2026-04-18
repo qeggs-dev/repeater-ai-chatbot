70cbbbca19f1abd23640f8b31af0f66d7b68ac89
Qeggs <get_boot@outlook.com>
2026-04-18 17:02:57 +0800

    feat(core): 添加Asteval工具支持
    - 在core/Model_Requester/tools模块中引入Asteval工具
    - 更新__all__列表以包含新的Asteval工具类

--- core/Model_Requester/tools/__init__.py
+++ core/Model_Requester/tools/__init__.py
@@ -2,9 +2,11 @@ from .demo import Demo
 from .http_requests import HTTPRequests
 from .set_prompt import SetPrompt
 from .metaso import Metaso
+from .asteval_tool import Asteval
 
 __all__ = [
     "HTTPRequests",
     "SetPrompt",
     "Metaso",
+    "Asteval",
 ]
