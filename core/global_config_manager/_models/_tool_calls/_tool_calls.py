5fb0117b3b0882acdcbc447c184ffc987cd5ce17
Qeggs <get_boot@outlook.com>
2026-04-27 05:42:19 +0800

    fix(core): 修正工具调用配置中的数据类型定义
    将 registed 字段的数据类型从 list[str] 更改为 set[str]，
    以确保注册工具的唯一性并优化性能。

--- core/global_config_manager/_models/_tool_calls/_tool_calls.py
+++ core/global_config_manager/_models/_tool_calls/_tool_calls.py
@@ -4,7 +4,7 @@ from typing import Literal
 
 class ToolCallsConfigs(BaseModel):
     enabled: bool = True
-    registed: list[str] = Field(default_factory=list)
+    registed: set[str] = Field(default_factory=list)
     allow_by_default: bool = False
     result_max_length_for_logs: int | None = 100
     allowed_http_methods: list[HTTPMethods] | Literal["ALL"] | None = None
