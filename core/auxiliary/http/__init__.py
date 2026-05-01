8ff3bdfd5457be466db32d77ec26470877b81e84
Qeggs <get_boot@outlook.com>
2026-05-01 22:17:05 +0800

    refactor(core/auxiliary/http): 重构HTTP相关模块结构
    将HTTP相关的限流、超时和SSL功能从core/http目录迁移至
    core/auxiliary/http目录，以更好地组织代码结构并更新
    __init__.py文件暴露相应的API接口。

--- core/auxiliary/http/__init__.py
+++ core/auxiliary/http/__init__.py
@@ -1,1 +1,12 @@
+from ._limit import ClientLimits
+from ._timeout import ClientTimeout
+from ._ssl import get_ssl_context, set_ssl_context
 from ._http_code import HTTPCode
+
+__all__ = [
+    "ClientLimits",
+    "ClientTimeout",
+    "get_ssl_context",
+    "set_ssl_context",
+    "HTTPCode",
+]
