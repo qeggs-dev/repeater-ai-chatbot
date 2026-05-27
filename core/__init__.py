0f3660f7596852dcc4bd54cb1b0badeca558ba23
Qeggs <get_boot@outlook.com>
2026-05-27 14:08:38 +0800

    refactor(core): 移除core模块中不必要的导出项
    移除了__version__、__author__、__license__、__copyright__等不再需要的元数据
    以及多个内部组件的直接导出，简化了core模块的接口暴露

--- core/__init__.py
+++ core/__init__.py
@@ -14,38 +14,8 @@ from .repeater_main import (
 from .api import root_router
 
 __all__ = [
-    "__version__",
-    "__author__",
-    "__license__",
-    "__copyright__",
     "Core",
     "Server",
     "RepeaterMain",
-    "root_router",
-    "admin_api_key_manager",
-    "api",
-    "assist_struct",
-    "auxiliary",
-    "call_api",
-    "context_manager",
-    "data_manager",
-    "global_config_manager",
-    "licenses_loader",
-    "lifespan",
-    "logger_init",
-    "markdown_render",
-    "model_api",
-    "model_requester",
-    "nexus_client",
-    "pools",
-    "repeater_response",
-    "repeater_traceback",
-    "request_log",
-    "server",
-    "special_exception"
-    "static_resources_client",
-    "status_map",
-    "text_buffer",
-    "text_template_processer",
-    "user_config_manager"
+    "root_router"
 ]
