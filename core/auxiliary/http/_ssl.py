23fc5c96dedcb38adcd1443009fb0bee3f6b08d5
Qeggs <get_boot@outlook.com>
2026-05-30 17:21:59 +0800

    feat(http): 添加传输层支持并重构SSL上下文管理
    - 引入RepeaterTransport类用于HTTP传输处理
    - 将SSL_CONTEXT重构为SSLContext类，提供更好的上下文管理
    - 添加ssl_context实例和相关方法的全局访问
    - 更新__init__.py导出新添加的模块和功能

--- core/auxiliary/http/_ssl.py
+++ core/auxiliary/http/_ssl.py
@@ -1,14 +1,28 @@
 import ssl
 
-SSL_CONTEXT: ssl.SSLContext = ssl.create_default_context()
+class SSLContext:
+    def __init__(self):
+        self.ssl_context: ssl.SSLContext = ssl.create_default_context()
+
+    def get_ssl_context(self):
+        return self.ssl_context
+    
+    def set_ssl_context(self, ssl_context: ssl.SSLContext):
+        self.ssl_context = ssl_context
+
+    def update_ssl_context(self):
+        self.ssl_context = ssl.create_default_context()
+
+ssl_context = SSLContext()
 
 def get_ssl_context():
-    return SSL_CONTEXT
+    global ssl_context
+    return ssl_context.get_ssl_context()
 
-def set_ssl_context(ssl_context: ssl.SSLContext):
-    global SSL_CONTEXT
-    SSL_CONTEXT = ssl_context
+def set_ssl_context(new_ssl_context: ssl.SSLContext):
+    global ssl_context
+    ssl_context.set_ssl_context(new_ssl_context)
 
 def update_ssl_context():
-    global SSL_CONTEXT
-    SSL_CONTEXT = get_ssl_context()
+    global ssl_context
+    ssl_context.update_ssl_context()
