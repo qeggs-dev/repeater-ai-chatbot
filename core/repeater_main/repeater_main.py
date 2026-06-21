ed8c5d36c77fcc8d6256d8880ba0a661d10660b3
Qeggs <get_boot@outlook.com>
2026-06-22 07:54:27 +0800

    feat(core): 添加方法文档注释并优化类型提示
    添加了详细的方法文档字符串说明，包括参数、返回值和功能描述，
    同时增加了类型提示以提高代码可读性和维护性。

--- core/repeater_main/repeater_main.py
+++ core/repeater_main/repeater_main.py
@@ -31,11 +31,19 @@ class RepeaterMain:
 
     @classmethod
     def get_now_server(cls) -> Server:
+        """
+        Get the current server instance
+        """
         if cls._now_server is None:
             raise RuntimeError("Server not inited")
         return cls._now_server
 
     def load_configs(self):
+        """
+        Load configs from file
+
+        Tip: The program needs a configuration file to boot, please load the configuration file first.
+        """
         config_loader = ConfigManager()
         path = self.env.path("CONFIG_DIR", Path("./configs/project_configs"))
         force_load_list = self.env.json("CONFIG_FORCE_LOAD_LIST", None)
@@ -50,6 +58,11 @@ class RepeaterMain:
         )
     
     def init_server(self, configs: GlobalConfigs):
+        """
+        Init the server
+
+        :param configs: GlobalConfigs
+        """
         host = "0.0.0.0" # 默认监听所有地址
         port = 8000 # 默认监听8000端口
 
@@ -92,6 +105,13 @@ class RepeaterMain:
         self.server_initer.init_middleware()
     
     def check_package(self, configs: GlobalConfigs):
+        """
+        Check that the package meets the requirements
+        
+        **Warning: it will be discarded**
+
+        :param configs: GlobalConfigs
+        """
         logger.info("Checking Packages...")
         start_check_packages_time = time.perf_counter_ns()
         check_package_list(
@@ -107,6 +127,11 @@ class RepeaterMain:
         self.server_initer.init_logger()
     
     def init_all(self, configs: GlobalConfigs):
+        """
+        One-click handles most initialization.
+
+        :param configs: GlobalConfigs
+        """
         logger.info(f"Run With Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
         logger.info(f"Core Version: {__version__}")
 
@@ -122,17 +147,28 @@ class RepeaterMain:
             init_resource_time = (end_init_resource_time - start_init_resource_time) / 1e6
         )
     
-    def set_inited_flag(self):
+    def set_inited_flag(self) -> None:
+        """
+        Set the inited flag of the server initer.
+
+        Important! The program needs to set this Flag to start.
+        """
         self.server_initer.set_inited_flag()
     
-    async def run_server(self):
+    async def run_server(self) -> int:
+        """
+        Run the server in async mode.
+        """
         RepeaterMain._now_server = self.server
         try:
             return await self.server.run_server()
         finally:
             RepeaterMain._now_server = None
 
-    def run(self):
+    def run(self) -> int:
+        """
+        Run the server.
+        """
         logger.info("Server starting...")
         try:
             return asyncio.run(
