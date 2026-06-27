af66cc7802998d218ed1d41d6e826035b5468d48
Qeggs <get_boot@outlook.com>
2026-06-27 13:45:25 +0800

    feat(core): 添加任务ID生成和日志记录功能
    - 当task_id为None时自动生成UUID作为任务ID
    - 将快开始任务的日志移动到最开始
    - 将 task_id 传递给 TaskLifespan 上下文管理器
    - 调整日志记录位置，在进入 Tasking 状态时记录任务信息

--- core/core/_core.py
+++ core/core/_core.py
@@ -304,6 +304,16 @@ class Core:
             # 记录开始时间
             task_start_time = TimeStamp()
 
+            if task_id is None:
+                task_id = uuid.uuid4()
+
+            logger.info("====================================", user_id = user_id)
+            logger.info(
+                "Start Task {task_id}",
+                user_id = user_id,
+                task_id = task_id,
+            )
+
             # 获取用户锁对象
             lock = await self._get_namespace_lock(user_id)
 
@@ -322,6 +332,7 @@ class Core:
             # 进入RUL执行
             async with TaskLifespan(
                 user_id = user_id,
+                task_id = task_id,
                 rul = lock,
                 enable_rul = enable_rul,
                 runtime = self.runtime
@@ -330,15 +341,14 @@ class Core:
 
                 # 进入状态
                 with task_status_stack.enter("Tasking"):
+                    logger.info(
+                        "Enter Task {task_id}",
+                        user_id = user_id,
+                        task_id = task_lifespan.task_id_str
+                    )
                 
                     with task_status_stack.enter("Prepareing"):
                         prepare_start_time = TimeStamp()
-                        logger.info("====================================", user_id = user_id)
-                        logger.info(
-                            "Start Task {task_id}",
-                            user_id = user_id,
-                            task_id = task_lifespan.task_id,
-                        )
 
                         # region [Checking Blacklist]
                         with task_status_stack.enter("Checking Blacklist"):
